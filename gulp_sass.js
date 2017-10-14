e00528857318f950b82cd3f147f911dfa02e0e85
AndrewHani <andro_hani@hotmail.com>
2017-10-14 17:15:13 +0200

    Updating Sass Task

--- gulp_sass.js
+++ gulp_sass.js
@@ -9,6 +9,8 @@ gulp.task('sass', function () {
    .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function () {
- gulp.watch('./sass/*.scss', ['sass']);
-});
+gulp.task('sass', function () {
+  return gulp.src('./sass/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+ });
