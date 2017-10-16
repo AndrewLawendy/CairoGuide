04b79f0dc81df022fbe0d74592a412bfbe3f85ec
AndrewHani <andro_hani@hotmail.com>
2017-10-17 00:06:54 +0200

    Updating Carousel

--- JS/CairoGuideMain.js
+++ JS/CairoGuideMain.js
@@ -5,21 +5,46 @@ $(document).ready(function () {
     var carouselBasePos = 0;
     var carouselPosDiff = 0;
     var carouselScrollLeft = 0;
-    $('.carousel-body').css('width', carouselItemCount + 'vw');
-    $('.carousel-container').on('mousedown', function (e) {
-        carouselDrag = true;
-        var _this = $(this);
-        carouselBasePos = e.pageX;
-        carouselScrollLeft = $(this).scrollLeft();
-    });
-    $('.carousel-container').on('mousemove', function (e) {
-        if(carouselDrag){
-            carouselPosDiff = carouselBasePos - e.pageX;
-            $(this).scrollLeft(carouselScrollLeft + carouselPosDiff);
-        }
-    });
+    if (carouselItemCount > 1) {
+        $('.carousel-body').css('width', carouselItemCount + '00vw');
+        $('.carousel-container').on('mousedown', function (e) {
+            carouselDrag = true;
+            var _this = $(this);
+            carouselBasePos = e.pageX;
+            carouselScrollLeft = $(this).scrollLeft();
+        });
+        $('.carousel-container').on('mousemove', function (e) {
+            if (carouselDrag) {
+                carouselPosDiff = carouselBasePos - e.pageX;
+                $(this).scrollLeft(carouselScrollLeft + carouselPosDiff);
+            }
+        });
+    }
 
     $(document).on('mouseup', function () {
-        carouselDrag = false;
+        //Carousel Autocomplete
+        if (carouselItemCount > 1 && carouselPosDiff != undefined) {
+            carouselDrag = false;
+            carouselScrollLeft = $('.carousel-container').scrollLeft();
+            var fullScroll = $('.carousel-item:first-child').width();
+            var scrollRemaining = fullScroll - (carouselPosDiff % fullScroll);
+            if (Math.abs(carouselPosDiff) > 50) {
+                if (carouselPosDiff > 0) {
+                    $('.carousel-container').animate({
+                        scrollLeft: fullScroll * Math.round((carouselScrollLeft + scrollRemaining) / fullScroll)
+                    }, 800);
+                } else {
+                    $('.carousel-container').animate({
+                        scrollLeft: fullScroll * Math.round((carouselScrollLeft - scrollRemaining) / fullScroll)
+                    }, 800);
+                }
+
+            } else {
+                $('.carousel-container').animate({
+                    scrollLeft: fullScroll * Math.round((carouselScrollLeft - carouselPosDiff) / fullScroll)
+                });
+            }
+            carouselPosDiff = undefined
+        }
     });
 });
