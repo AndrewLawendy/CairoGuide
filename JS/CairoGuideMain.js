4bee63ae7f0b15bb9d49a704474eb83d7234d89f
AndrewHani <andro_hani@hotmail.com>
2017-10-16 21:06:00 +0200

    Carousel

--- JS/CairoGuideMain.js
+++ JS/CairoGuideMain.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    console.log('Document is ready');
+    //Carousel
+    var carouselItemCount = $('.carousel-item').length;
+    var carouselDrag = false;
+    var carouselBasePos = 0;
+    var carouselPosDiff = 0;
+    var carouselScrollLeft = 0;
+    $('.carousel-body').css('width', carouselItemCount + 'vw');
+    $('.carousel-container').on('mousedown', function (e) {
+        carouselDrag = true;
+        var _this = $(this);
+        carouselBasePos = e.pageX;
+        carouselScrollLeft = $(this).scrollLeft();
+    });
+    $('.carousel-container').on('mousemove', function (e) {
+        if(carouselDrag){
+            carouselPosDiff = carouselBasePos - e.pageX;
+            $(this).scrollLeft(carouselScrollLeft + carouselPosDiff);
+        }
+    });
+
+    $(document).on('mouseup', function () {
+        carouselDrag = false;
+    });
 });
