b8a876c162666c85af6d98a6b2f9f4cb7d8a3369
Andrew Gorgui <Andrew.Gorgui@linkdev.com>
2017-10-19 12:01:20 +0200

    Carousel Almost Complete

--- JS/CairoGuideMain.js
+++ JS/CairoGuideMain.js
@@ -6,10 +6,16 @@ $(document).ready(function () {
     var carouselPosDiff = 0;
     var carouselScrollLeft = 0;
     if (carouselItemCount > 1) {
+        carouselItemCount +=2;
         $('.carousel-body').css('width', carouselItemCount + '00vw');
+        $('.carousel-item:last').clone().prependTo('.carousel-body');
+        $('.carousel-item:nth-child(2)').clone().appendTo('.carousel-body');
+        fullScroll = $('.carousel-item:first-child').width();
+        $('.carousel-container').scrollLeft(fullScroll);
         $('.carousel-container').on('mousedown', function (e) {
             if (e.which == 1) {
                 carouselDrag = true;
+                fullScroll = $('.carousel-item:first-child').width();
                 carouselItemTarget = $(e.target).closest('.carousel-item');
                 var test = $('.carousel-data').not($(carouselItemTarget).find('.carousel-data'));
                 carouselBasePos = e.pageX;
@@ -18,18 +24,31 @@ $(document).ready(function () {
         });
         $('.carousel-container').on('mousemove', function (e) {
             if (carouselDrag) {
+                entered = false;
+                var activeItemIndex = carouselItemTarget.index();
                 carouselPosDiff = carouselBasePos - e.pageX;
-                $(this).scrollLeft(carouselScrollLeft + carouselPosDiff);
-                carouselItemTarget.find('.carousel-data').css({transform:'translateX('+(carouselPosDiff*-1.2)+'px)'})
+                carouselItemTarget.find('.carousel-data').css('left',30+(carouselPosDiff*-1.2)+'px');
                 if(carouselPosDiff>0){
-                    carouselItemTarget.next().find('.carousel-data').addClass('titleEnter-left');
-                    $('.titleEnter-left').css({transform:'translateX('+(1000-(carouselPosDiff*.5))+'px)'})
+                    if(activeItemIndex == (carouselItemCount-1) && !entered){
+                        $('.carousel-container').scrollLeft(fullScroll);
+                        carouselScrollLeft = fullScroll;
+                        carouselItemTarget = $('.carousel-item:nth-child(2)')
+                        entered = true;
+                    }
+                    carouselItemTarget.next().find('.carousel-data').css('left',fullScroll-(carouselPosDiff*.5)+'px');
+                }else{
+                    if(activeItemIndex == 0 && !entered){
+                        $('.carousel-container').scrollLeft((carouselItemCount-2)*fullScroll);
+                        activeScrollItem = $('.carousel-item:nth-last-child(2)')
+                        carouselScrollLeft = (carouselItemCount-2)*fullScroll;
+                        entered = true;
+                    }
+                    carouselItemTarget.prev().find('.carousel-data').css('left',-650-(carouselPosDiff*.5)+'px');
                 }
+                $(this).scrollLeft(carouselScrollLeft + carouselPosDiff);
             }
         });
         var moveNext = function(){
-            var actualScroll = $('.carousel-container').scrollLeft();
-            var fullScroll = $('.carousel-item:first-child').width();
             var carouselArray = $('.carousel-item');
             for(var i = 0; i<carouselArray.length;i++){
                 if($(carouselArray[i]).position().left == 0){
@@ -37,22 +56,57 @@ $(document).ready(function () {
                     break;
                 }
             }
-            activeScrollItem.find('.carousel-data').animate({transform:'translateX(-650px)'},1200)
-            activeScrollItem.next().find('.carousel-data').css('transform','translateX(1000px)').delay(300).queue(function(){
-                $(this).animate({transform:'translateX(0)'}).dequeue();
-            });;
-            $('.carousel-container').stop().animate({scrollLeft:fullScroll * Math.round((actualScroll+fullScroll)/fullScroll)},1600);
-            //$('.section-title').css({transform:'translateX(0px)'});
+            var activeItemIndex = (activeScrollItem).index();
+            if(activeItemIndex == (carouselItemCount-1)){
+                $('.carousel-container').scrollLeft(fullScroll);
+                activeScrollItem = $('.carousel-item:nth-child(2)')
+            }
+            var actualScroll = $('.carousel-container').scrollLeft();
+            fullScroll = $('.carousel-item:first-child').width();
+            activeScrollItem.find('.carousel-data').animate({left:'-650'},1200);
+            activeScrollItem.next().find('.carousel-data').css('left',fullScroll+'px').delay(300).animate({left:30},1200);
+            $('.carousel-container').stop().animate({scrollLeft:fullScroll * Math.round((actualScroll+fullScroll)/fullScroll)},1600,function(){
+                $('.carousel-data').css('left','30px');
+            });
+        }
+
+        var movePrevious = function(){
+            var carouselArray = $('.carousel-item');
+            for(var i = 0; i<carouselArray.length;i++){
+                if($(carouselArray[i]).position().left == 0){
+                    var activeScrollItem = $(carouselArray[i]);
+                    break;
+                }
+            }
+            var activeItemIndex = (activeScrollItem).index();
+            if(activeItemIndex == 0){
+                $('.carousel-container').scrollLeft((carouselItemCount-2)*fullScroll);
+                activeScrollItem = $('.carousel-item:nth-last-child(2)')
+            }
+            var actualScroll = $('.carousel-container').scrollLeft();
+            fullScroll = $('.carousel-item:first-child').width();
+            activeScrollItem.find('.carousel-data').animate({left:fullScroll},1000);
+            activeScrollItem.prev().find('.carousel-data').css('left','-650px').delay(500).animate({left:30},1200);
+            $('.carousel-container').stop().animate({scrollLeft:fullScroll * Math.round((actualScroll-fullScroll)/fullScroll)},1600,function(){
+                $('.carousel-data').css('left','30px');
+            });
         }
 
         $('#next').on('click',moveNext);
 
-        $('#previous').on('click',function(){
-            var actualScroll = $('.carousel-container').scrollLeft();
-            var fullScroll = $('.carousel-item:first-child').width();
-            $('.carousel-container').stop().animate({scrollLeft:fullScroll * Math.round((actualScroll-fullScroll)/fullScroll)},1600);
+        $('#previous').on('click',movePrevious);
+
+        Automatic Scroll
+        var carouselAuto = setInterval(moveNext,4000);
+        $('.carousel-container').on('mouseenter',function(){
+            clearInterval(carouselAuto);
+        });
+        $('.carousel-container').on('mouseleave',function(){
+            carouselAuto = setInterval(moveNext,4000);
         });
     }
+    // End of Carousel
+
 
     $(document).on('mouseup', function (e) {
         if (e.which == 1) {
@@ -67,11 +121,12 @@ $(document).ready(function () {
                         $('.carousel-container').animate({
                             scrollLeft: fullScroll * Math.round((carouselScrollLeft + scrollRemaining) / fullScroll)
                         }, 800);
+
                     } else {
                         $('.carousel-container').animate({
                             scrollLeft: fullScroll * Math.round((carouselScrollLeft - scrollRemaining) / fullScroll)
                         }, 800);
-                        $('.section-title').css({transform:'translateX(0px)'})
+                        carouselItemTarget.find('.carousel-data').animate({left:fullScroll},800);
                     }
 
                 } else {
@@ -79,8 +134,11 @@ $(document).ready(function () {
                         scrollLeft: fullScroll * Math.round((carouselScrollLeft - carouselPosDiff) / fullScroll)
                     });
                 }
-                carouselPosDiff = undefined
+                carouselPosDiff = undefined;
+                entered = false;
+                $('.carousel-data').animate({left:30},800);
             }
+            //End of Carousel Autocomplete
         }
     });
 });
